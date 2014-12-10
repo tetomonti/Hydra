db82e4d04abf28360aed47e5e677ca90cce4c4a4
montilab <montilab@bu.edu>
2014-12-10 13:13:21 -0500

    Added Vinays latest version, so that we have one final version we can all use.

--- tools/RNASeq_pipeline/helper.py
+++ tools/RNASeq_pipeline/helper.py
@@ -1,38 +1,3 @@
-#  Copyright (c) 2014, Boston University. All rights reserved.
-#  
-#  Redistribution and use in source and binary forms, with or without
-#  modification, are permitted provided that the following conditions are met: 
-#  
-#  1. Redistributions of source code must retain the above copyright notice, this
-#     list of conditions and the following disclaimer. 
-#  2. Redistributions in binary form must reproduce the above copyright notice,
-#     this list of conditions and the following disclaimer in the documentation
-#     and/or other materials provided with the distribution. 
-#  
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-#  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-#  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-#  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-#  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#  
-#  The views and conclusions contained in the software and documentation are those
-#  of the authors and should not be interpreted as representing official policies, 
-#  either expressed or implied, of Boston University.
-#  
-#  Authors:
-#    Daniel Gusenleitner [1,2], Vinay Kartha [1,2], Francesca Mulas [2], 
-#    Yuxiang Tan [1,2], Liye Zhang [2], Stefano Monti [1,2]
-#
-#  [1] Bioinformatics Program, Boston University
-#  [2] Center for Computational Biomedicine, Boston University  
-#  
-
-
 import os, sys, shutil, json, time, shlex, subprocess, random 
 import qsub_module, single_cpu_module, module_helper
   
@@ -72,6 +37,7 @@ def initialize_standard(param):
     checkParameter(param,key='QC_and_trim_only',dType=bool)
     checkParameter(param,key='run_cufflinks',dType=bool)
     checkParameter(param,key='run_htseq',dType=bool)
+    checkParameter(param,key='run_featureCount',dType=bool)
     
     #checking working directory and going there
     checkParameter(param,key='working_dir',dType=str,checkFile=True) 
