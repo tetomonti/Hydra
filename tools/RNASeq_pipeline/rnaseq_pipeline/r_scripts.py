1efb9085f313f81385c3cd6106a072fa76cc0854
Yann <yannpaul@bu.edu>
2015-03-17 14:04:19 -0400

    fixed path getting function

--- tools/RNASeq_pipeline/rnaseq_pipeline/r_scripts.py
+++ tools/RNASeq_pipeline/rnaseq_pipeline/r_scripts.py
@@ -3,4 +3,5 @@ import os.path
 
 def get_script_path(filename):
     '''return full path of R script stored with rnaseq_pipeline package'''
-    return os.path.join(__file__, 'r_scripts', filename)
+    package_dir = os.path.dirname(__file__)
+    return os.path.join(package_dir, 'r_scripts', filename)
