Fixed several bugs in the html report, among them the rotation of table headers in firefox and internet explorer, fixed broken links, fixed wrong percentage of assigned reads in featurecounts.

hydra_pkg/bamqc.py
@@ -205,7 +205,7 @@ def plot_alignments(param):
     fig.set_size_inches(fig_width, 8)
     index = np.arange(len(param['bamqc_stub']))
 
-    bar_width = fig_width / float(len(param['bamqc_stub'])) * 0.9
+    bar_width = fig_width / float(len(param['bamqc_stub'])) * 50
     opacity = 0.4
     rects1 = plt.bar(index,
                      total,
@@ -262,7 +262,7 @@ def plot_spliced_reads(param):
     fig.set_size_inches(fig_width, 8)
     index = np.arange(len(param['bamqc_stub']))
 
-    bar_width = fig_width / float(len(param['bamqc_stub'])) * 0.9
+    bar_width = fig_width / float(len(param['bamqc_stub'])) * 50
     opacity = 0.4
     _ = plt.bar(index, percent, bar_width, alpha=opacity, color='b')
     plt.xlabel('Samples')
@@ -297,7 +297,7 @@ def plot_insert_reads(param):
     fig.set_size_inches(fig_width, 8)
     index = np.arange(len(param['bamqc_stub']))
 
-    bar_width = fig_width / float(len(param['bamqc_stub'])) * 0.9
+    bar_width = fig_width / float(len(param['bamqc_stub'])) * 50
     opacity = 0.4
     _ = plt.bar(index, percent, bar_width, alpha=opacity, color='b')
     plt.xlabel('Samples')
@@ -331,7 +331,7 @@ def plot_delete_reads(param):
     fig.set_size_inches(fig_width, 8)
     index = np.arange(len(param['bamqc_stub']))
 
-    bar_width = fig_width / float(len(param['bamqc_stub'])) * 0.9
+    bar_width = fig_width / float(len(param['bamqc_stub'])) * 50
     opacity = 0.4
     _ = plt.bar(index, percent, bar_width, alpha=opacity, color='b')
     plt.xlabel('Samples')
@@ -367,7 +367,7 @@ def plot_paired_singleton(param):
     fig.set_size_inches(fig_width, 8)
     index = np.arange(len(param['bamqc_stub']))
 
-    bar_width = fig_width / float(len(param['bamqc_stub'])) * 0.9
+    bar_width = fig_width / float(len(param['bamqc_stub'])) * 50
     opacity = 0.4
     rects1 = plt.bar(index,
                      paired,
@@ -442,7 +442,7 @@ def plot_mismatches(param):
     fig.set_size_inches(fig_width, 8)
     index = np.arange(len(param['bamqc_stub']))
 
-    bar_width = fig_width / float(len(param['bamqc_stub'])) * 0.9
+    bar_width = fig_width / float(len(param['bamqc_stub'])) * 50
     opacity = 0.4
     rects1 = plt.bar(index, mm0, bar_width,
                      alpha=opacity, color='b')